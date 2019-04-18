9b5668dabd8d23165d65a47fad99e693f91e78a7
tschatzmann <tarrta2002@gmail.com>
2019-04-18 19:08:22 -0400

    Updates

---
+++ precache-manifest.daa19676b10cb1162a12364ac29af285.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91df3625fe1a7a30468c",
+    "url": "/clicky-game/static/css/main.1f106c2c.chunk.css"
+  },
+  {
+    "revision": "91df3625fe1a7a30468c",
+    "url": "/clicky-game/static/js/main.fb9a9f12.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "5183ab22499b1a114ba59133a62e4b59",
+    "url": "/clicky-game/index.html"
+  }
+];
